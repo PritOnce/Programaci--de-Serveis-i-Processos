4a28dae2ed446acc5d8db2d3a2e3aa950c2d8174
PritOnce11 <pritonce@gmail.com>
2023-11-09 23:54:09 +0100

    cambio en act 2-3

--- Actividad2-3/src/Rent.java
+++ Actividad2-3/src/Rent.java
@@ -2,17 +2,18 @@ import java.util.concurrent.Semaphore;
 
 public class Rent {
     public static void main(String[] args) throws Exception {
-        //creao el semaforo con la cantidad de coches que pueden haber
-        //en el parking al mismo tiempo
         Semaphore semaphore = new Semaphore(3);
-        Coches [] coches = new Coches[5];
+        Coches [] coches = new Coches[10];
         Caja[] caja = new Caja[coches.length];
+
+        for(int e = 0; e < caja.length; e++){
+            caja[e] = new Caja();
+        }
 
         for(int i = 0; i < coches.length ; i++){
             Thread t = new Thread(new Coches(semaphore, i, caja[i]));
             t.start();
         }
-
     }
 }
 
@@ -22,6 +23,7 @@ class Coches implements Runnable{
     private Semaphore semaphore;
     private int id;
     private Caja caja;
+    private static int cochesSalidos = 0;
 
     public Coches(Semaphore semaphore, int id, Caja caja){
         this.semaphore = semaphore;
@@ -31,26 +33,37 @@ class Coches implements Runnable{
 
     @Override
     public void run() {
-        try {
-            
-            System.out.println("El coche: " +id+ " esta en la fila");
+    try {
+        
+        while (true) {
+            long tiempo = (long) ((Math.random() * 5000) + 1000);
+            System.out.println("El coche: " + id + " esta en la fila");
             semaphore.acquire();
-            System.out.println("El coche: " +id+ " ha entrado");
-            Thread.sleep((long) ((Math.random()*9000)+1000));
-            System.out.println("El coche " +id+ " ha salido a circular");
+            System.out.println("El coche: " + id + " ha entrado");
+            Thread.sleep( tiempo );
+            System.out.println("El coche " + id + " ha salido a circular");
             semaphore.release();
-            System.out.println("El coche: " +id+ " acumula " +caja.getPrecio()+ " Euros");
             caja.setPrecio(5);
-        } catch (InterruptedException ie) {
-            System.out.println("Se ha interrumpido la ejecucion");
-        }catch(Exception e){
-            System.out.println("Error general");
+            cochesSalidos++;
+            System.out.println("El coche: " + id + " acumula " + caja.getPrecio() + " Euros");
+            Thread.sleep(tiempo);
+            if(cochesSalidos >= 5){
+                System.out.println(cochesSalidos);
+                System.exit(0);
+            }
         }
+
+    } catch (InterruptedException ie) {
+        System.out.println("Se ha interrumpido la ejecucion");
+    } catch (Exception e) {
+        System.out.println("Error general");
     }
+}
 
 }
 
 class Caja{
+    public int length;
     private int precio;
 
     public synchronized int getPrecio() {

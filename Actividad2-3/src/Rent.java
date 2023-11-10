72ac420089b5f20c17be0472b3cea78cf8808a56
PritOnce <ngojlia2@nigul.cide.es>
2023-11-10 18:26:22 +0100

    comentarios 2-3

--- Actividad2-3/src/Rent.java
+++ Actividad2-3/src/Rent.java
@@ -2,14 +2,22 @@ import java.util.concurrent.Semaphore;
 
 public class Rent {
     public static void main(String[] args) throws Exception {
+        /*
+        Creo un semaforo con 3 espacios que sera el maximo espacio que tendrá mi parking
+        Creo 10 coches, pero no todos acabarán entrando o saliendo
+        Y una caja por cada coche que hay
+        */
         Semaphore semaphore = new Semaphore(3);
         Coches [] coches = new Coches[10];
         Caja[] caja = new Caja[coches.length];
 
+        //Creo todas las cajas por coche
         for(int e = 0; e < caja.length; e++){
             caja[e] = new Caja();
         }
 
+        //Inicio el for para generar los coches(Hilos) y le paso el semaforo
+        //el i para darle un id y una caja a cada uno
         for(int i = 0; i < coches.length ; i++){
             Thread t = new Thread(new Coches(semaphore, i, caja[i]));
             t.start();
@@ -20,6 +28,10 @@ public class Rent {
 
 class Coches implements Runnable{
 
+    /*
+    Creo las variables y ademas coches salidos para controlar el while de mas adelante
+    y no dejar en bucle el programa
+    */
     private Semaphore semaphore;
     private int id;
     private Caja caja;
@@ -31,11 +43,15 @@ class Coches implements Runnable{
         this.caja = caja;
     }
 
+    //Inicio los hilos
     @Override
     public void run() {
     try {
         
+        //Habro un while para continuar lo que yo quiera y controlarlo despues con la variabel cochesSalidos
         while (true) {
+            //Genero un numero random para darle un tiempo aleatorio a lo que esta el coche en el parking
+            //y para que espere para volver a entrar si "quiere"
             long tiempo = (long) ((Math.random() * 5000) + 1000);
             System.out.println("El coche: " + id + " esta en la fila");
             semaphore.acquire();
@@ -43,12 +59,14 @@ class Coches implements Runnable{
             Thread.sleep( tiempo );
             System.out.println("El coche " + id + " ha salido a circular");
             semaphore.release();
+            //Sumo 5 al contador de cada coche que haya salido
+            //Y sumo 1 para controlar cuantos coches han salido
             caja.setPrecio(5);
             cochesSalidos++;
             System.out.println("El coche: " + id + " acumula " + caja.getPrecio() + " Euros");
-            Thread.sleep(tiempo);
+            Thread.sleep(tiempo);//Controla el tiempo que el coche está "circulando"
+            //Cuando cochesSalidos llega a 5 el while finaliza y acaba
             if(cochesSalidos >= 5){
-                System.out.println(cochesSalidos);
                 System.exit(0);
             }
         }
@@ -62,14 +80,16 @@ class Coches implements Runnable{
 
 }
 
+
 class Caja{
-    public int length;
     private int precio;
 
-    public synchronized int getPrecio() {
+    //Mostrar lo recaudado por coche
+    public int getPrecio() {
         return precio;
     }
 
+    //Set que ira cambiando por cada coche que sale con synchronized para evitar la perdida de datos
     public synchronized void setPrecio(int coste) {
         precio = precio + coste;
     }
